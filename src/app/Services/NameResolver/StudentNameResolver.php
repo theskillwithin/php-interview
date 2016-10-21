b84e13d85dcb8b7739ffd5ff4c23bb8ee6cbaad9
theskillwithin <austinppeterson@gmail.com>
2016-10-20 20:59:47 -0600

    test first names

--- src/app/Services/NameResolver/StudentNameResolver.php
+++ src/app/Services/NameResolver/StudentNameResolver.php
@@ -4,10 +4,39 @@ namespace Services\NameResolver;
 class StudentNameResolver implements NameResolverInterface {
 
     public function resolve($users) {
+
+//        if(count(array_unique($users->firstName))<count($users->firstName))
+//        {
+//            echo "duplicate";
+//        }
+//        else {
+//            echo "not duplicate";
+//        }
+
+
         foreach ($users as $index => $user) {
-            echo $user->firstName;
+//            echo $user->firstName;
+//            echo $user->lastName;
+
+
+            $firstNames[] = $user->firstName;
+
+
+            $user->displayName = $user->firstName;
+            echo $user->displayName;
+            echo "<br>";
         }
-        var_dump($users);
+
+
+        for ($current = 0; $current < count($firstNames);$current++) {
+            if (in_array($firstNames[$current],$firstNames)) {
+                echo "duplicate";
+            } else {
+                echo "NONduplicate";
+            }
+        }
+
+        var_dump($firstNames);
     }
 
 }
